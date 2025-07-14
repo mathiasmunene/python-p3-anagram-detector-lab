a2112098e765f0ac3f5d7bad58417d375b07384d
Mathias Munene <munenemathias@gmail.com>
2025-07-14 21:25:34 +0300

    Update anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+# lib/anagram.py
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, possible_anagrams):
+        return [
+            candidate for candidate in possible_anagrams
+            if sorted(candidate.lower()) == sorted(self.word)
+            and candidate.lower() != self.word
+        ]
